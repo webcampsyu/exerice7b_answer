ea876811eb0739e967f7f41a4c7ff5e7a1e5a705
EC2 Default User <ec2-user@ip-172-31-37-43.ec2.internal>
2021-11-02 06:33:08 +0000

    feat: create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create(
+    name: "testuser#{n+1}",
+    email: "testuser#{n+1}@test.com",
+    password: "testuser#{n+1}",
+  )
+end
+
+
+# Userそれぞれに対して、10レコードずつBookを作成
+# 現在時刻から 0 ~ 15 までのランダムな数字を引いた時刻でBookを作成
+
+User.all.each do |user|
+  10.times do |n|
+    Book.create(
+      title: "testbook#{n+1}",
+      body: "testbook#{n+1}",
+      user_id: user.id,
+      created_at: Time.current - rand(15).day
+    )
+  end
+end
